92302827830da1e126e27e133cc0056e20daa39b
Chenhonxinh <2310816801@qq.com>
2022-02-14 14:24:19 +0800

    将ScrollTip部分封装成了组件

---
+++ src/components/ScrollTip.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="banner" :class="{'content-collapse':$store.state.collapse}">
+            <span class="scrollTip">{{scrollTip}}</span>
+    </div>
+</template>
+
+<script>
+import {ref }from 'vue'
+
+export default {
+  setup(){
+        let scrollTip=ref("这是一条提示信息!")
+        return{
+            scrollTip
+        }
+    }
+}
+</script>
+
+<style scoped>
+     .banner{
+        position: absolute;
+        box-sizing: border-box;
+        right: 0;
+        left: 160px;
+        height: 50px;
+        margin: 5px;
+        padding: 5px;
+        overflow: hidden;
+        line-height: 40px;
+        background-color: white;
+        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+        transition: left .5s ease;
+    }
+    .scrollTip{
+         position: absolute;
+         left: 101%;
+         white-space: nowrap;
+         overflow: hidden;
+         animation: scrollTip 10s linear 0s infinite;
+
+     }
+     @keyframes scrollTip {
+         from{ left: 101%;}
+         to{left: -10%;}
+     }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,7 @@
     <div class="about">
         <v-header></v-header>
         <v-sidebar></v-sidebar>
-        <div class="banner" :class="{'content-collapse':$store.state.collapse}">
-            <span class="scrollTip">{{scrollTip}}</span>
-        </div>
+        <v-scroll-tip></v-scroll-tip>
         <div class="content-box" :class="{'content-collapse':$store.state.collapse}">
             <router-view></router-view>
         </div>
@@ -15,15 +13,10 @@
 <script>
 import vHeader from '../components/Header.vue'
 import vSidebar from '../components/Sidebar.vue'
-import {ref }from 'vue'
+import vScrollTip from '../components/ScrollTip.vue'
 export default {
-    components:{vHeader,vSidebar},
-    setup(){
-        let scrollTip=ref("这是一条提示信息!")
-        return{
-            scrollTip
-        }
-    }
+    components:{vHeader,vSidebar,vScrollTip},
+  
 }
 </script>
 
@@ -37,34 +30,7 @@ export default {
         padding-bottom: 30px;
         transition: left .4s ease-in-out;
     }  
-     .banner{
-        position: absolute;
-        box-sizing: border-box;
-        right: 0;
-        left: 160px;
-        height: 50px;
-        margin: 5px;
-        padding: 5px;
-        overflow: hidden;
-        line-height: 40px;
-        background-color: white;
-        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-        transition: left .5s ease;
-    }
 
-    .content-collapse{
-        left: 70px;
-     } 
-     .scrollTip{
-         position: absolute;
-         left: 101%;
-         white-space: nowrap;
-         overflow: hidden;
-         animation: scrollTip 10s linear 0s infinite;
-
-     }
-     @keyframes scrollTip {
-         from{ left: 101%;}
-         to{left: -10%;}
-     }
+   
+     
 </style>
