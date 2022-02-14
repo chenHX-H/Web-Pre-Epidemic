d075dc1938b69a33fdfc55d6ab1c56b16bd0eccc
Chenhonxinh <2310816801@qq.com>
2022-02-14 14:07:12 +0800

    完成了滚动字幕功能

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,11 @@
     <div class="about">
         <v-header></v-header>
         <v-sidebar></v-sidebar>
+        <div class="banner" :class="{'content-collapse':$store.state.collapse}">
+            <span class="scrollTip">{{scrollTip}}</span>
+        </div>
         <div class="content-box" :class="{'content-collapse':$store.state.collapse}">
-                    <router-view></router-view>
-
+            <router-view></router-view>
         </div>
     </div>
    
@@ -13,8 +15,15 @@
 <script>
 import vHeader from '../components/Header.vue'
 import vSidebar from '../components/Sidebar.vue'
+import {ref }from 'vue'
 export default {
-    components:{vHeader,vSidebar}
+    components:{vHeader,vSidebar},
+    setup(){
+        let scrollTip=ref("这是一条提示信息!")
+        return{
+            scrollTip
+        }
+    }
 }
 </script>
 
@@ -26,10 +35,36 @@ export default {
         top: 70px;
         bottom: 0;
         padding-bottom: 30px;
-        background-color: #222228;
         transition: left .4s ease-in-out;
+    }  
+     .banner{
+        position: absolute;
+        box-sizing: border-box;
+        right: 0;
+        left: 160px;
+        height: 50px;
+        margin: 5px;
+        padding: 5px;
+        overflow: hidden;
+        line-height: 40px;
+        background-color: white;
+        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+        transition: left .5s ease;
     }
+
     .content-collapse{
         left: 70px;
-    }
+     } 
+     .scrollTip{
+         position: absolute;
+         left: 101%;
+         white-space: nowrap;
+         overflow: hidden;
+         animation: scrollTip 10s linear 0s infinite;
+
+     }
+     @keyframes scrollTip {
+         from{ left: 101%;}
+         to{left: -10%;}
+     }
 </style>
